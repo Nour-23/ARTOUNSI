9810e2b8b1c073d9b39b629455daba6b07da6deb
aniaduor <minar.idouas6@gmail.com>
2025-03-02 21:15:29 +0100

    feat: create mailer service

---
+++ src/Service/MailerService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class MailerService
+{
+    private MailerInterface $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    public function sendEmail(string $to, string $subject, string $content): void
+    {
+        $email = (new Email())
+            ->from('noreply@example.com') // Change this to your sender email
+            ->to($to)
+            ->subject($subject)
+            ->text($content)
+            ->html('<p>' . $content . '</p>');
+
+        $this->mailer->send($email);
+    }
+}
