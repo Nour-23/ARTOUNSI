665503a3f35e77013e31d1fbfe32b0b88aa17129
sarah <sarraoueslati34@gmaiil.com>
2025-03-03 22:24:41 +0100

    api + metiers

--- src/Controller/FrontarticleController.php
+++ src/Controller/FrontarticleController.php
@@ -11,6 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/articlefront')]
 final class FrontarticleController extends AbstractController
